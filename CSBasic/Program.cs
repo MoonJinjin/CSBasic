71890f72f4547bd693c4b2c347629ac239b85494
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-04-20 11:53:29 +0900

    #1 기본 용어 실습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -8,8 +8,31 @@ namespace CSBasic
 {
     class Program
     {
+        // var 키워드는 지역변수로만 사용 가능
         static void Main(string[] args)
         {
+            // keyword
+            // int as;
+
+            // context keyword
+            var name = "함기훈";
+
+            // 식별자 Identifier
+            int alpha;
+            int Alpha;
+            // int break;
+            // int 2abc;
+            // int has space;
+            // int 😘;
+            int 한글숫자;
+
+            // inline comment
+            /*
+             * comment
+             */
+
+
+            Console.WriteLine("Hello");
         }
     }
 }

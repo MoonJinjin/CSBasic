5e8c909167457929e24b4cc29814b4f0bda9ef6d
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-05-06 09:26:52 +0900

    #13 키보드 입력 및 분기 처리

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -10,6 +10,51 @@ namespace CSBasic2
     {
         static void Main(string[] args)
         {
+            Console.Write("입력:");
+            string line = Console.ReadLine();
+            if (line.Contains("안녕"))
+            {
+                Console.WriteLine("너도 안녕~");
+            }else if (line.Contains("바보"))
+            {
+                Console.WriteLine("너가 바보야!");
+            }else if (line.Contains("밥"))
+            {
+                Console.WriteLine("배고파~");
+            }
+            else
+            {
+                Console.WriteLine("심심해~");
+            }
+
+            ConsoleKeyInfo info;
+            bool isLoop = true;
+            while (isLoop)
+            {
+                info = Console.ReadKey();
+                switch (info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.Write("↑");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.Write("↓");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Write("→");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.Write("←");
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("으악!");
+                        isLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nUnknown key");
+                        break;
+                }
+            }
         }
     }
 }

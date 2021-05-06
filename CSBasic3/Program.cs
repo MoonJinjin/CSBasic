414e440e1850d80cad1555a589d668a3d37a0c01
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-05-06 09:55:07 +0900

    초기 커밋.

---
+++ CSBasic3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray.Length);
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+            // Console.WriteLine(intArray[100]);
+            // Console.WriteLine(intArray[-1]);
+        }
+    }
+}

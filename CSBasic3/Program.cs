b3b7d7db2846b27bf0780cdf51e9f9a3e05c20e5
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-05-13 08:54:40 +0900

    #19 역for문 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -19,7 +19,7 @@ namespace CSBasic3
 
             int i = 0;
             int[] intArray2 = { 52, 273, 32, 65, 103 };
-            while(i < intArray2.Length)
+            while (i < intArray2.Length)
             {
                 Console.WriteLine(i + "번째 출력 : " + intArray2[i]);
                 i++;
@@ -31,6 +31,21 @@ namespace CSBasic3
                 Console.WriteLine("입력 (종료 : exit) :");
                 input = Console.ReadLine();
             } while (input != "exit");
+
+            for (int j = '가'; j <= '힣'; j++)
+            {
+                Console.Write((char)j);
+            }
+
+            int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+            for (int k = intArray3.Length - 1; k >= 0; k--)
+            {
+                Console.WriteLine(intArray3[k]);
+            }
+            for (int k = intArray3.Length; k > 0; k--)
+            {
+                Console.WriteLine(intArray3[k - 1]);
+            }
         }
     }
 }
